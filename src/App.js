063de6dd74ec412099f26caab6927bb1562af751
Ali Hakak <ali.hakak@gmail.com>
2020-05-04 12:23:45 -0400

    Architecture + Category fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainHeader from './components/AppHeader';
+import CategoryContainer from './containers/CategoryContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MainHeader/>
       </header>
+        <CategoryContainer/>
     </div>
   );
 }
