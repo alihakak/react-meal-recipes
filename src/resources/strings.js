daa20654f08da017cb33735759a4a0aa0ff6598c
Ali Hakak <ali.hakak@gmail.com>
2020-05-04 22:52:05 -0400

    Added files

---
+++ src/resources/strings.js
@@ -0,0 +1,7 @@
+export const appStrings = {
+    headerText: 'Delicious recipes from around the world...',
+    category: {
+        headerText: "What do you feel like? ",
+        headerIntro: "Please select your favorite meal category, we'll help you cook it!"
+    }
+}
